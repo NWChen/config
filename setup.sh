3a61920fdcc32ac7e6154ffcee8a7a10e7f6e1ed
Neil Chen <neilwchen@gmail.com>
2019-04-26 12:09:20 -0400

    tell em

--- setup.sh
+++ setup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-cd ~;
+source chrome.sh
+source docker.sh
+source gimp.sh
+source git.sh
+source nodejs.sh
+source spotify.sh
+source i3.sh
+source pip.sh
+source ros.sh
+source go.sh
+source zsh.sh
 
-. chrome.sh
-. docker.sh
-. gimp.sh
-. git.sh
-. nodejs.sh
-. spotify.sh
-. i3.sh
-. pip.sh
-. ros.sh
-. go.sh
-. zsh.sh
+chrome
+docker
+gimp
+git
+nodejs
+spotify
+i3
+pip
+ros
+go
+zsh
 
 echo "Please restart now."
